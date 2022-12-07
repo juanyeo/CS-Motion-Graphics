26a3ccd682eb81be9bbe75ff673d5452a75509ed
juanyeo <juanyeo@naver.com>
2022-12-07 17:39:28 +0900

    Particle - 물리엔진 입자 단위

---
+++ Particle.py
@@ -0,0 +1,9 @@
+# Particle
+
+class Particle:
+    def __init__(self):
+        self.x = [0, 0, 0]
+        self.v = [0, 0, 0]
+        self.f = [0, 0, 0]
+        self.m = 0
+        self.neighbors = []
