c0ba5d73325f52312bdb1c9a22d1d371472b6c6f
juanyeo <juanyeo@naver.com>
2022-12-16 09:27:58 +0900

    Force 추상클래스 상속

--- ExternalForce.py
+++ ExternalForce.py
@@ -1,7 +1,8 @@
+from Force import Force
 import numpy as np
 from MatrixUtil import *
 
-class ExternalForce:
+class ExternalForce(Force):
     def __init__(self, m, x1, x2):
         self.weight = m
         self.edge_x1 = np.array(x1)
@@ -9,7 +10,7 @@ class ExternalForce:
         self.delta_t = 0.008
         self.momentum = self.estimateMomentum()
         self.mid_distance = 1.
-        self.epsilon = 0.01
+        self.epsilon = 0.1 # 0.01
 
     def detectContact(self, particleSystem):
         midpoint = np.array([0., 0., 0.])
@@ -42,3 +43,19 @@ class ExternalForce:
         v = (self.edge_x2 - self.edge_x1) / self.delta_t
         
         return self.weight * v
+    
+    def getForce(self, particleSystem):
+        F = self.momentum / self.delta_t
+
+        for i in range(len(particleSystem.particles)):
+            particle = particleSystem.particles[i]
+
+            distance = getParticleDistance(self.edge_x2, particle.x)
+            closeness = (self.mid_distance - distance) / self.mid_distance
+
+            if (closeness < 0):
+                continue
+
+            particle.f += self.epsilon * closeness * F
+        
+        return particleSystem
